bde8d6dc1c0ff0406ccce478fbc1615225f8d794
Lin <gulin365@gmail.com>
2021-06-30 00:00:49 +0800

    Fix fonts bug

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     //设置全局字体
     QFontDatabase::addApplicationFont(":/Fonts/YuppySC-Regular.ttf");
-    QFont font("YuppySC-Regular", 12);
+    QFont font("Yuppy SC", 12);
     a.setFont(font);
 
     MainScene w;

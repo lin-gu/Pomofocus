77faef811a05806aceace727ac0deae8980df7c4
Lin <gulin365@gmail.com>
2021-06-29 23:44:18 +0800

    Add font in resources

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include "Scenes/mainscene.h"
 
 #include <QApplication>
+#include <QFontDatabase>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //设置全局字体
+    QFontDatabase::addApplicationFont(":/Fonts/YuppySC-Regular.ttf");
+    QFont font("YuppySC-Regular", 12);
+    a.setFont(font);
+
     MainScene w;
     w.show();
     return a.exec();
